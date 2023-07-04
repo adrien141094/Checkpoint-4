4794178da29512b8fea8a3ff8a282d35bd548453
ADRIEN TURCEK_PERRON <adrien.turcekperron@gmail.com>
2023-07-04 23:54:56 +0200

    Mise en plas des config min pour le projet + mise en place de la bibliotèque Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import Home from "./pages/Home";
-
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <h1 className=' bg-green-400 font-bold text-4xl'>Hello mon gars !</h1>
+      <p className=" bg-red-800">coucou</p>
     </div>
   );
 }
