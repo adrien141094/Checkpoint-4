a8d8ac3f935c1c4ed8a890523d6e3f33eaee2a26
ADRIEN TURCEK_PERRON <adrien.turcekperron@gmail.com>
2023-07-19 10:21:27 +0200

    modif text lorem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className=' bg-green-400 font-bold text-4xl'>Hello mon gars !</h1>
-      <p className=" bg-red-800">coucou</p>
+      <h1 className=" bg-green-400 font-bold text-4xl">Hello mon gars !</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
+        perferendis, mollitia tenetur aliquid dolores voluptas amet voluptatem
+        repellendus incidunt, eaque nisi harum explicabo saepe omnis modi neque!
+        Distinctio reprehenderit inventore vel incidunt quisquam, est harum
+        adipisci consequuntur debitis atque? Rem unde magni officia eaque!
+        Aspernatur aut repellat magnam iste assumenda!
+      </p>
     </div>
   );
 }
